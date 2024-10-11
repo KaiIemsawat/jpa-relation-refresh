827a83d2ba1bab6706299c97f62b63fec33da346
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-10-11 14:58:31 -0400

    initial setup and student entity

---
+++ src/main/java/com/kai/spring_data_jpa_demo/entity/Student.java
@@ -0,0 +1,33 @@
+package com.kai.spring_data_jpa_demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(
+        name = "tbl_student"
+        , uniqueConstraints = @UniqueConstraint(name = "emailid_unique", columnNames = "email_address")
+)
+public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    private Long studentId;
+    private String firstname;
+    private String lastname;
+
+    @Column(name = "email_address", nullable = false)
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+
+}
